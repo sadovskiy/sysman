11075e13aaf3ff87c0cfa4b50c2e1ee22974e731
Б. С. Садовский <sadovskiy@mgsu.ru>
2014-07-23 23:11:47 +0400

    Добавли файл перевода.

---
+++ test_ru_RU.ts
@@ -0,0 +1,305 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>DialogConnectToDataBase</name>
+    <message>
+        <location filename="dialogconnecttodatabase.ui" line="23"/>
+        <source>Dialog</source>
+        <translation>Соединение с Базой</translation>
+    </message>
+    <message>
+        <location filename="dialogconnecttodatabase.ui" line="33"/>
+        <source>Set User Name</source>
+        <translation>Введите имя пользователя</translation>
+    </message>
+    <message>
+        <location filename="dialogconnecttodatabase.ui" line="40"/>
+        <source>sysmanadmin</source>
+        <translation>sysmanadmin</translation>
+    </message>
+    <message>
+        <location filename="dialogconnecttodatabase.ui" line="51"/>
+        <source>Set Password</source>
+        <translation>Введите пароль</translation>
+    </message>
+    <message>
+        <location filename="dialogconnecttodatabase.ui" line="58"/>
+        <source>40^NmKL</source>
+        <translation>40^NmKL</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Главное окно</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <source>List Documents</source>
+        <translation>Список документов</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <source>Document - All</source>
+        <translation>Все документы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <source>Input Document</source>
+        <translation>Входящие</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="118"/>
+        <source>Output Document</source>
+        <translation>Исходящие</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="144"/>
+        <source>All Table</source>
+        <oldsource>Tab 1</oldsource>
+        <translation>Вся таблица</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source>Students</source>
+        <translation>Список студентов</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="172"/>
+        <source>Orders</source>
+        <translation>Приказы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="186"/>
+        <source>Payment</source>
+        <translation>Оплата</translation>
+    </message>
+    <message>
+        <source>Tab 2</source>
+        <translation type="obsolete">Стеденты</translation>
+    </message>
+    <message utf8="true">
+        <source>Страница</source>
+        <translation type="obsolete">Приказы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <source>Connect</source>
+        <translation>Соединение</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="263"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="266"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <source>id</source>
+        <translation type="obsolete">ИД</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <location filename="mainwindow.cpp" line="144"/>
+        <source>order_admission_num</source>
+        <translation>Номер приказа о зачислении</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="87"/>
+        <location filename="mainwindow.cpp" line="145"/>
+        <source>order_dismissal_num</source>
+        <translation>Номер приказа об отчислении</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <location filename="mainwindow.cpp" line="146"/>
+        <source>date_of_order_admission</source>
+        <translation>Дата приказа о зачислении</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <location filename="mainwindow.cpp" line="147"/>
+        <source>date_of_order_dismissal</source>
+        <translation>Дата приказа об отчислении</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <location filename="mainwindow.cpp" line="149"/>
+        <source>amount</source>
+        <translation>Сумма оплаты по договору</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="91"/>
+        <location filename="mainwindow.cpp" line="150"/>
+        <source>semester</source>
+        <translation>Семестр</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="mainwindow.cpp" line="151"/>
+        <source>discount</source>
+        <translation>Скидка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="93"/>
+        <location filename="mainwindow.cpp" line="152"/>
+        <source>actual_amount_of_payment</source>
+        <translation>Сумма фактической оплаты</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="153"/>
+        <source>date_of_pay</source>
+        <translation>День оплаты</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="95"/>
+        <location filename="mainwindow.cpp" line="154"/>
+        <source>penalties</source>
+        <translation>Пени</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <location filename="mainwindow.cpp" line="121"/>
+        <source>contract_number</source>
+        <translation>Номер договора</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <location filename="mainwindow.cpp" line="122"/>
+        <source>contract_date</source>
+        <translation>Дата заключения договора</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="100"/>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>contract_type</source>
+        <translation>Тип договора</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>surname</source>
+        <translation>Фамилия</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="103"/>
+        <location filename="mainwindow.cpp" line="126"/>
+        <source>patronym</source>
+        <translation>Отчество</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <location filename="mainwindow.cpp" line="127"/>
+        <source>Sex</source>
+        <translation>Пол</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="105"/>
+        <location filename="mainwindow.cpp" line="128"/>
+        <source>date_of_birth</source>
+        <translation>Дата рождения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <location filename="mainwindow.cpp" line="129"/>
+        <source>place_of_birth</source>
+        <translation>Место рождения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="107"/>
+        <location filename="mainwindow.cpp" line="130"/>
+        <source>citizenship</source>
+        <translation>Гражданство</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>passport_series</source>
+        <translation>Серия паспорта</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="109"/>
+        <location filename="mainwindow.cpp" line="132"/>
+        <source>passport_number</source>
+        <translation>Номер паспорта</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <location filename="mainwindow.cpp" line="133"/>
+        <source>passport_authority</source>
+        <translation>Кем выдан паспорт</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <location filename="mainwindow.cpp" line="134"/>
+        <source>passport_date_of_issue</source>
+        <translation>Дата выдачи паспорта</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="135"/>
+        <source>passport_subdivision_code</source>
+        <translation>Код подразделения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="113"/>
+        <location filename="mainwindow.cpp" line="136"/>
+        <source>registration_adm_unit</source>
+        <translation>Место регистрации</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="114"/>
+        <location filename="mainwindow.cpp" line="137"/>
+        <source>registration_adress</source>
+        <translation>Адрес регистрации</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="115"/>
+        <location filename="mainwindow.cpp" line="138"/>
+        <source>registration_date</source>
+        <translation>Дата регистрации</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="116"/>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source>postcode</source>
+        <translation>Почтовый индекс</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="117"/>
+        <location filename="mainwindow.cpp" line="140"/>
+        <source>phone</source>
+        <translation>Телефон</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <location filename="mainwindow.cpp" line="141"/>
+        <source>email</source>
+        <translation>Электронная почта</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="119"/>
+        <location filename="mainwindow.cpp" line="142"/>
+        <source>individual_taxpayer_identification_number</source>
+        <translation>ИНН</translation>
+    </message>
+</context>
+</TS>
